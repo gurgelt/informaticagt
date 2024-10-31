d8917169f2983f63802ca25be33a5fa3bc246301
gurgelt <paulo.gtcanal@gmail.com>
2024-10-31 15:32:49 -0300

    Adicionado Loading

---
+++ js/loading.js
@@ -0,0 +1,8 @@
+let elem_preloader = document.getElementById("preloader");
+let elem_loader = document.getElementById("loader");
+console.log(" Ok");
+
+setTimeout(function () {
+  elem_preloader.classList.remove("preloader");
+  elem_loader.classList.remove("loader");
+}, 1280);
